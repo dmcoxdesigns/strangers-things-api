a67905713a56a9501e87fa915608ed6d069c9431
dmcoxdesigns <dmcoxdesigns@gmail.com>
2023-01-19 17:21:06 -0700

    ReactDom render update

---
+++ src/api/index.js
@@ -0,0 +1,4 @@
+export const BASE_URL =
+  "https://strangers-things.herokuapp.com/api/2209-FTB-MT-WEB-PT";
+export ReactDOM;
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from "react";
+/* eslint-disable no-unreachable */
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const resp = await fetch(
+        "https://strangers-things.herokuapp.com/api/2209-FTB-MT-WEB-PT/posts"
+      );
+      const data = await resp.json();
+
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      <h1>Posts</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <div>{post.body}</div>
+        </div>
+      ))}
+    </>
+  );
+  root.render(<App />, document.getElementById("root"));
+};
